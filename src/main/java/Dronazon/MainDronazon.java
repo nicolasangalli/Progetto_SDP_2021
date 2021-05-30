77914e604d38c736dc1bbee1bd77051bf4921314
nicolasangalli <nicolasangalli93@gmail.com>
2021-05-30 16:23:02 +0200

    Modified the Dronazone orders generation
    - handled the case where the starting point was the same as the finish point;
    - removed orderlist, useless;

--- src/main/java/Dronazon/MainDronazon.java
+++ src/main/java/Dronazon/MainDronazon.java
@@ -5,7 +5,6 @@ import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
-import java.util.ArrayList;
 import java.util.Random;
 
 
@@ -13,13 +12,11 @@ public class MainDronazon {
 
     private static int orderIdCount = 0;
     private static Gson gson;
-    private static ArrayList<Order> orderList;
     private static MqttClient dronazon;
     private static String pubTopic = "dronazon/smartcity/orders";
     private static int qos = 2;
 
     public static void main(String[] args) {
-        orderList = new ArrayList<>();
         gson = new Gson();
         String broker = "tcp://localhost:1883";
         String clientId = MqttClient.generateClientId();
@@ -52,13 +49,16 @@ public class MainDronazon {
         Random random = new Random();
         Coordinate startPoint = new Coordinate(random.nextInt(10), random.nextInt(10));
         Coordinate finishPoint = new Coordinate(random.nextInt(10), random.nextInt(10));
+        while(startPoint.getX() == finishPoint.getX() && startPoint.getY() == finishPoint.getY()) { //start point == finish point
+            finishPoint.setX(random.nextInt(10));
+            finishPoint.setY(random.nextInt(10));
+        }
         Order o = new Order(id, startPoint, finishPoint);
-        orderList.add(o);
         orderIdCount++;
 
-        System.out.println("\nGenerated new order: " + o.getId() + ": ");
+        System.out.println("\nGenerated new order:");
+        System.out.println("id: " + o.getId());
         System.out.println("(" + o.getStartPoint().getX() + "," + o.getStartPoint().getY() + ") -> (" + o.getFinishPoint().getX() + "," + o.getFinishPoint().getY() + ")");
-
 
         MqttMessage message = new MqttMessage(gson.toJson(o).getBytes());
         message.setQos(qos);
